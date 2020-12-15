daa0db0fc606d05a1d991e78981e8c62d60374a1
Ryan Kaneshiro <kaneshiro.ryan@gmail.com>
2020-12-14 21:32:24 -0800

    Add solution for day 14 part 2

--- 2020/14/day14/src/main.rs
+++ 2020/14/day14/src/main.rs
@@ -1,11 +1,11 @@
 use std::env;
 use std::fs;
 use std::collections::hash_map::HashMap;
+use std::collections::hash_set::HashSet;
 use regex::Regex;
 
 fn apply_mask(mask_template: &str, value: u64) -> u64 {
     let mut masked_value: u64 = value;
-
     // Set bits in the mask based on the template
     for (i, c) in mask_template.chars().enumerate() {
         let shift = mask_template.len() - i - 1;
@@ -19,6 +19,52 @@ fn apply_mask(mask_template: &str, value: u64) -> u64 {
     }
 
     return masked_value;
+}
+
+fn float_index(mask_template: &str, index: u64) -> HashSet<u64> {
+    let mut x_index: Vec<usize> = Vec::new();
+    // First pin the 1's in the mask to 1 and collect the bit index of all the X's
+    let mut masked_value: u64 = index;
+    for (i, c) in mask_template.chars().enumerate() {
+        let shift = mask_template.len() - i - 1;
+        // println!("shift: {}, i: {}, c: {}", shift, i, c);
+        if c == '1' {
+            masked_value |= 1 << shift;
+        } else if c == 'X' {
+            x_index.push(shift);
+        }
+    }
+
+    // println!("current masked value: {}, {:b}", masked_value, masked_value);
+
+    // Now go through and set the bits in the X positions
+    let mut indices: HashSet<u64> = HashSet::new();
+    let combinations = u64::pow(2, x_index.len() as u32);
+
+    // Reversing isn't necessary since we are going over all bit combinations anyway
+    x_index.reverse();
+
+    // println!("x_index: {:?}", x_index);
+
+    for val in 0..combinations {
+        let mut new_index: u64 = masked_value;
+        // println!("val: {}, {:b}", val, val);
+        for (i, x) in x_index.iter().enumerate() {
+            let val_bit: u64 = (val & (1 << i)) >> i;
+            let val_shifted = 1 << x;
+            // println!("val_bit: {}, shift: {}, val_shifted: {:b}, new_index: {:b}", val_bit, x, val_shifted, new_index);
+            if val_bit == 1 {
+                new_index |= val_shifted;
+            } else {
+                new_index &= !val_shifted;
+            }
+        }
+        println!("new_index: {}, {:b}", new_index, new_index);
+
+        indices.insert(new_index);
+    }
+
+    return indices;
 }
 
 fn main() {
@@ -52,8 +98,10 @@ fn main() {
                 .parse::<u64>()
                 .unwrap();
 
-            println!("index: {}, value: {}, mask: {}", index, value, mask);
-            mem.insert(index, apply_mask(mask, value));
+            for new_index in float_index(mask, index) {
+                println!("index: {}, value: {}, mask: {}", new_index, value, mask);
+                mem.insert(new_index, value);
+            }
        }
     }
 
