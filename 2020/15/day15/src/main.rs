2a734880f05c8e0a16af323a6339f83b09762b0f
Ryan Kaneshiro <kaneshiro.ryan@gmail.com>
2020-12-15 21:46:49 -0800

    Lame solution to day 15 part 2
    Remove excess prints and do a cargo run --release
    instead.

--- 2020/15/day15/src/main.rs
+++ 2020/15/day15/src/main.rs
@@ -10,15 +10,16 @@ fn main() {
 
     for (i, g) in input.iter().enumerate() {
         let mut guess_turn = VecDeque::new();
-        guess_turn.push_front(i as u32);
+        guess_turn.push_front((i + 1) as u32);
 
         guesses.insert(*g, guess_turn);
     }
 
     println!("Initial guess map: {:?}", guesses);
-    let limit = 2020;
+    // let limit = 2020;
+    let limit = 30_000_000;
     let mut most_recent: u32 = *input.iter().last().unwrap();
-    for turn in guesses.len()..limit {
+    for turn in guesses.len() + 1..limit + 1 {
         let mut guess_turn = guesses.entry(most_recent).or_insert(VecDeque::new());
 
         // println!("Most recent guess {}, Previous answers {:?}", most_recent, guess_turn);
@@ -30,8 +31,18 @@ fn main() {
 
         guess_turn = guesses.entry(guess).or_insert(VecDeque::new());
         guess_turn.push_front(turn as u32);
+        if guess_turn.len() > 2 {
+            guess_turn.pop_back();
+        }
 
-        println!("Guess on turn {} is {}", turn + 1, guess);
+        // println!("Guess on turn {} is {}", turn, guess);
+        // let mut blargh: Vec<(&u32, &VecDeque<u32>)> = guesses.iter().collect();
+        // blargh.sort_by_key(|x| x.0);
+        // for (guess, turns) in &blargh {
+            // println!("  {}: {:?}", guess, turns);
+        // }
         most_recent = guess;
     }
+
+    println!("Guess {} on turn {}", most_recent, limit);
 }
