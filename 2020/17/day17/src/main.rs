e51228054effe51545c3c432b787762e4c4bacb4
Ryan Kaneshiro <kaneshiro.ryan@gmail.com>
2020-12-19 11:16:41 -0800

    Added day 17 part 2 solution

--- 2020/17/day17/src/main.rs
+++ 2020/17/day17/src/main.rs
@@ -2,57 +2,65 @@ use std::env;
 use std::fs;
 use std::collections::hash_set::HashSet;
 
-fn bounds(active_cells: &HashSet<(i32, i32, i32)>) -> ((i32, i32), (i32, i32), (i32, i32)) {
-    let mut items: Vec<(i32, i32, i32)> = active_cells.iter().map(|(x, y, z)| (*x, *y, *z)).collect();
+fn bounds(active_cells: &HashSet<(i32, i32, i32, i32)>) -> ((i32, i32), (i32, i32), (i32, i32), (i32, i32)) {
+    let mut items: Vec<(i32, i32, i32, i32)> = active_cells.iter().map(|(x, y, z, w)| (*x, *y, *z, *w)).collect();
 
-    items.sort_by_key(|(x, _, _)| *x);
+    items.sort_by_key(|(x, _, _, _)| *x);
 
-    let x_bounds = (items.first().unwrap_or(&(0,0,0)).0, items.last().unwrap_or(&(0,0,0)).0 + 1);
+    let origin = (0,0,0,0);
 
-    items.sort_by_key(|(_, y, _)| *y);
+    let x_bounds = (items.first().unwrap_or(&origin).0, items.last().unwrap_or(&origin).0 + 1);
 
-    let y_bounds = (items.first().unwrap_or(&(0,0,0)).1, items.last().unwrap_or(&(0,0,0)).1 + 1);
+    items.sort_by_key(|(_, y, _, _)| *y);
 
-    items.sort_by_key(|(_, _, z)| *z);
-    let z_bounds = (items.first().unwrap_or(&(0,0,0)).2, items.last().unwrap_or(&(0,0,0)).2 + 1);
+    let y_bounds = (items.first().unwrap_or(&origin).1, items.last().unwrap_or(&origin).1 + 1);
 
-    return (x_bounds, y_bounds, z_bounds);
+    items.sort_by_key(|(_, _, z, _)| *z);
+    let z_bounds = (items.first().unwrap_or(&origin).2, items.last().unwrap_or(&origin).2 + 1);
+
+    items.sort_by_key(|(_, _, _, w)| *w);
+    let w_bounds = (items.first().unwrap_or(&origin).3, items.last().unwrap_or(&origin).3 + 1);
+    return (x_bounds, y_bounds, z_bounds, w_bounds);
 }
 
-fn print_space(active_cells: &HashSet<(i32, i32, i32)>) {
+fn print_space(active_cells: &HashSet<(i32, i32, i32, i32)>) {
     // Find min, max for each axis
-    let (x_bounds, y_bounds, z_bounds) = bounds(active_cells);
+    let (x_bounds, y_bounds, z_bounds, w_bounds) = bounds(active_cells);
 
     for z in z_bounds.0..z_bounds.1 {
-        println!("z={}", z);
-        for y in y_bounds.0..y_bounds.1 {
-            let row: Vec<char> = (x_bounds.0..x_bounds.1)
-                .map(|x| match active_cells.contains(&(x, y, z)) {
-                    true => '#',
-                    false => '.'
-                })
-                .collect();
+        for w in w_bounds.0..w_bounds.1 {
+            println!("z={},w={}", z, w);
+            for y in y_bounds.0..y_bounds.1 {
+                let row: Vec<char> = (x_bounds.0..x_bounds.1)
+                    .map(|x| match active_cells.contains(&(x, y, z, w)) {
+                        true => '#',
+                        false => '.'
+                    })
+                    .collect();
 
-            println!("{:?}", row);
+                println!("{:?}", row);
+            }
         }
     }
 }
 
-fn active_neighbors(active_cells: &HashSet<(i32, i32, i32)>, x: i32, y: i32, z: i32) -> u32 {
+fn active_neighbors(active_cells: &HashSet<(i32, i32, i32, i32)>, x: i32, y: i32, z: i32, w: i32) -> u32 {
     let mut total: u32 = 0;
 
     for cell_x in x-1..x+2 {
         for cell_y in y-1..y+2 {
             for cell_z in z-1..z+2 {
-                if cell_x == x && cell_y == y && cell_z == z {
-                    // Skip the center point. Only want neighbors
-                    continue;
-                }
+                for cell_w in w-1..w+2 {
+                    if cell_x == x && cell_y == y && cell_z == z && cell_w == w {
+                        // Skip the center point. Only want neighbors
+                        continue;
+                    }
 
-                // println!("  Checking neighbor: {},{},{}", cell_x, cell_y, cell_z);
+                    // println!("  Checking neighbor: {},{},{}", cell_x, cell_y, cell_z);
 
-                if active_cells.contains(&(cell_x, cell_y, cell_z)) {
-                    total += 1;
+                    if active_cells.contains(&(cell_x, cell_y, cell_z, cell_w)) {
+                        total += 1;
+                    }
                 }
             }
         }
@@ -61,28 +69,30 @@ fn active_neighbors(active_cells: &HashSet<(i32, i32, i32)>, x: i32, y: i32, z: 
     return total;
 }
 
-fn next_step(active_cells: &HashSet<(i32, i32, i32)>) -> HashSet<(i32, i32, i32)> {
-    let mut next: HashSet<(i32, i32, i32)> = HashSet::new();
+fn next_step(active_cells: &HashSet<(i32, i32, i32, i32)>) -> HashSet<(i32, i32, i32, i32)> {
+    let mut next: HashSet<(i32, i32, i32, i32)> = HashSet::new();
 
-    let (x_bounds, y_bounds, z_bounds) = bounds(&active_cells);
+    let (x_bounds, y_bounds, z_bounds, w_bounds) = bounds(&active_cells);
 
     for x in x_bounds.0 - 1..x_bounds.1 + 2 {
         for y in y_bounds.0 - 1..y_bounds.1 + 2 {
             for z in z_bounds.0 - 1..z_bounds.1 + 2 {
-                let cell = (x, y, z);
-                let is_curr_active = active_cells.contains(&cell);
-                let neighbors = active_neighbors(&active_cells, x, y, z);
-                println!(" cell: {:?}, currently active: {}, active neighbors: {}", cell, is_curr_active, neighbors);
-                let is_new_active: bool = match (is_curr_active, neighbors) {
-                    (true, 2) => true,
-                    (true, 3) => true,
-                    (false, 3) => true,
-                    _ => false
-                };
+                for w in w_bounds.0 - 1..w_bounds.1 + 2 {
+                    let cell = (x, y, z, w);
+                    let is_curr_active = active_cells.contains(&cell);
+                    let neighbors = active_neighbors(&active_cells, x, y, z, w);
+                    // println!(" cell: {:?}, currently active: {}, active neighbors: {}", cell, is_curr_active, neighbors);
+                    let is_new_active: bool = match (is_curr_active, neighbors) {
+                        (true, 2) => true,
+                        (true, 3) => true,
+                        (false, 3) => true,
+                        _ => false
+                    };
 
-                println!("  new state is {}", is_new_active);
-                if is_new_active {
-                    next.insert(cell);
+                    // println!("  new state is {}", is_new_active);
+                    if is_new_active {
+                        next.insert(cell);
+                    }
                 }
             }
         }
@@ -100,7 +110,7 @@ fn main() {
     println!("Opening {}", filename);
 
     let data = fs::read_to_string(filename).expect("Unable to read file");
-    let mut active_cells: HashSet<(i32, i32, i32)> = data
+    let mut active_cells: HashSet<(i32, i32, i32, i32)> = data
         .split("\n")
         .enumerate()
         .filter(|(_, s)| !s.is_empty())
@@ -108,8 +118,8 @@ fn main() {
             .chars()
             .enumerate()
             .filter(|(_, c)| *c == '#')
-            .map(|(x, _)| (x as i32, y as i32, 0))
-            .collect::<Vec<(i32, i32, i32)>>())
+            .map(|(x, _)| (x as i32, y as i32, 0, 0))
+            .collect::<Vec<(i32, i32, i32, i32)>>())
         .flatten()
         .collect();
 
