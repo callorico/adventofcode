c2b20777536dc148cc8d624e717477055174567e
Ryan Kaneshiro <kaneshiro.ryan@gmail.com>
2020-12-24 10:09:02 -0800

    Add solution for day 22 part 2

--- 2020/22/day22/src/main.rs
+++ 2020/22/day22/src/main.rs
@@ -3,8 +3,18 @@ use std::env;
 use std::fs;
 
 
+struct Counter {
+    value: u32
+}
+
+fn next_value(counter: &mut Counter) -> u32 {
+    let next = counter.value + 1;
+    counter.value = next;
+    return next;
+}
+
+
 fn winner(hands: &Vec<(&str, VecDeque<i32>)>) -> Option<usize> {
-    /// Return the position of the winning hand, if any.
     let non_empty: Vec<(usize, _)> = hands.iter()
         .enumerate()
         .filter(|(_, pair)| pair.1.len() > 0)
@@ -17,12 +27,115 @@ fn winner(hands: &Vec<(&str, VecDeque<i32>)>) -> Option<usize> {
     }
 }
 
+
 fn score(hand: &VecDeque<i32>) -> u32 {
     return hand.iter()
         .rev()
         .enumerate()
         .map(|(i, c)| (i as u32 + 1) * (*c as u32))
         .sum();
+}
+
+
+fn matches(hands: &Vec<(&str, VecDeque<i32>)>, prior_rounds: &Vec<Vec<VecDeque<i32>>>) -> bool {
+    for prior in prior_rounds.iter() {
+
+        let mut found_match = true;
+        for ((x, h1), h2) in hands.iter().zip(prior.iter()) {
+            // println!("{}. Current {:?}. Previous: {:?}", x, h1, h2);
+            if h1.len() != h2.len() || !h1.iter().zip(h2.iter()).all(|(c1, c2)| c1 == c2) {
+                found_match = false;
+                break;
+            }
+        }
+
+        if found_match {
+            return true;
+        }
+
+    }
+
+    return false;
+}
+
+
+fn combat(hands: &mut Vec<(&str, VecDeque<i32>)>, game_counter: &mut Counter) -> usize {
+    let mut prior_rounds: Vec<Vec<VecDeque<i32>>> = Vec::new();
+
+    let game = next_value(game_counter);
+
+    println!("\n=== Game {} ===\n", game);
+
+    return loop {
+        let round = prior_rounds.len() + 1;
+        println!("-- Round {} (Game {}) --", round, game);
+
+        for (player_id, deck) in hands.iter() {
+            println!("{}'s deck: {:?}", player_id, deck);
+        }
+
+        // Return index 0 if there was a previous round with the same cards.
+        if matches(hands, &prior_rounds) {
+            println!("The hands match a previous round. {} is the winner!", hands[0].0);
+            return 0;
+        }
+
+        let round_copy: Vec<VecDeque<i32>> = hands.iter().map(|(_, c)| c.clone()).collect();
+        prior_rounds.push(round_copy);
+
+        let mut cards: Vec<(usize, i32)> = hands.iter_mut()
+            .enumerate()
+            .map(|(i, (_, h))| (i, h.pop_front().unwrap()))
+            .collect();
+        for (index, c) in &cards {
+            println!("{} plays: {}", hands[*index].0, c);
+        }
+
+        // Check to see if we need to recurse
+        let winner_index: usize;
+        if cards.iter().all(|(i, c)| hands[*i].1.len() >= (*c as usize)) {
+            // Recursive combat time!
+            println!("Playing a sub-game to determine the winner...");
+
+            let mut hands_copy: Vec<(&str, VecDeque<i32>)> = Vec::new();
+            for (i, count) in &cards {
+                hands_copy.push(
+                    (
+                        hands[*i].0,
+                        hands[*i].1
+                            .iter()
+                            .map(|i| *i)
+                            .take(*count as usize)
+                            .collect()
+                    )
+                )
+            }
+
+            winner_index = combat(&mut hands_copy, game_counter);
+            println!("\n...anyway, back to game {}", game);
+        } else {
+            // otherwise, the winner if the player with the highest card
+            cards.sort_by_key(|(_, c)| -(*c));
+            let (i, _) = cards.iter().next().unwrap();
+            winner_index = *i;
+        }
+
+        println!("{} wins round {} of game {}!", hands[winner_index].0, round, game);
+
+        // Winners card goes first.
+        cards.sort_by_key(|(index, _)| (*index != winner_index));
+        for (_, c) in &cards {
+            hands[winner_index].1.push_back(*c);
+        }
+
+        let win = winner(&hands);
+        if win.is_some() {
+            println!("The winner of game {} is {}!", game, hands[winner_index].0);
+            break win.unwrap();
+        }
+
+        println!("");
+    }
 }
 
 fn main() {
@@ -55,40 +168,9 @@ fn main() {
 
     println!("Parsed hands: {:?}", hands);
 
-    let mut round: u32 = 1;
-
-    let winning_hand: &VecDeque<i32> = loop {
-
-        println!("--- Round {} ---", round);
-
-        for (player_id, deck) in &hands {
-            println!("{}'s deck: {:?}", player_id, deck);
-        }
-
-        let mut cards: Vec<(usize, i32)> = hands.iter_mut()
-            .enumerate()
-            .map(|(i, (_, h))| (i, h.pop_front().unwrap()))
-            .collect();
-        for (index, c) in &cards {
-            println!("{} plays: {}", hands[*index].0, c);
-        }
-
-        cards.sort_by_key(|(_, c)| -(*c));
-        let (i, _) = cards.iter().next().unwrap();
-        println!("{} wins the round!", hands[*i].0);
-
-        for (_, c) in &cards {
-            hands[*i].1.push_back(*c);
-        }
-
-        let win = winner(&hands).map(|i| &hands[i].1);
-        if win.is_some() {
-            break win.unwrap();
-        }
-
-        println!("");
-        round += 1;
-    };
+    let mut counter = Counter { value: 0 };
+    let winning_index = combat(&mut hands, &mut counter);
+    let winning_hand: &VecDeque<i32> = &hands[winning_index].1;
 
     println!("Score: {}", score(winning_hand));
 }
