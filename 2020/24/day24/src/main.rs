c0a7a0af0e7b9a19084c4637e57b086733deeef5
Ryan Kaneshiro <kaneshiro.ryan@gmail.com>
2020-12-24 21:51:47 -0800

    Add solution for day 24 part 2

--- 2020/24/day24/src/main.rs
+++ 2020/24/day24/src/main.rs
@@ -3,6 +3,47 @@ use std::env;
 use std::collections::hash_set::HashSet;
 use std::collections::hash_map::HashMap;
 
+fn neighbors(dir_deltas: &HashMap<&str, (i32, i32)>, cell: (i32, i32)) -> Vec<(i32, i32)> {
+    let mut adjacent: Vec<(i32, i32)> = Vec::new();
+    for (dx, dy) in dir_deltas.values() {
+        adjacent.push((cell.0 + dx, cell.1 + dy));
+    }
+
+    return adjacent;
+}
+
+fn next_step(dir_deltas: &HashMap<&str, (i32, i32)>, black_tiles: &HashSet<(i32, i32)>) -> HashSet<(i32, i32)> {
+    let mut all_neighbors: HashSet<(i32, i32)> = black_tiles.clone();
+    for t in black_tiles.iter() {
+        all_neighbors.extend(neighbors(dir_deltas, *t));
+    }
+
+    let mut new_black_tiles: HashSet<(i32, i32)> = HashSet::new();
+
+    for t in all_neighbors.iter() {
+        let num_black_neighbors: usize = neighbors(dir_deltas, *t)
+            .iter()
+            .filter(|n| black_tiles.contains(*n))
+            .count();
+
+        let is_black = black_tiles.contains(t);
+        let is_still_black: bool = match (is_black, num_black_neighbors) {
+            (true, 0) => false,
+            (true, 1) => true,
+            (true, 2) => true,
+            (true, _) => false,
+            (false, 2) => true,
+            _ => false
+        };
+
+        if is_still_black {
+            new_black_tiles.insert(*t);
+        }
+    }
+
+    return new_black_tiles;
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
@@ -54,4 +95,9 @@ fn main() {
 
     println!("Black tiles: {:?}", black_tiles);
     println!("Total black tiles: {}", black_tiles.len());
+
+    for day in 1..=100 {
+        black_tiles = next_step(&dir_deltas, &black_tiles);
+        println!("Day {}: {}", day, black_tiles.len());
+    }
 }
