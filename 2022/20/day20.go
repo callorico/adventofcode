c669d6ef4066960dc82c6f11865b9d7a95afcfe3
Ryan Kaneshiro <kaneshiro.ryan@gmail.com>
2023-01-22 17:20:48 -0800

    Add solution for day 20 part 2

--- 2022/20/day20.go
+++ 2022/20/day20.go
@@ -17,27 +17,37 @@ func check(e error) {
 func PrintRing(ring *ring.Ring) {
 	var i = 0
 	ring.Do(func(p any) {
-		fmt.Printf("%d: %d\n", i, p.(int))
+		fmt.Printf("%d: %d\n", i, p.(int64))
 		i++
 	})
 }
 
-func Shift(r *ring.Ring, n int, ringSize int) {
+func Shift(r *ring.Ring, n int64, ringSize int) {
 	if n == 0 {
 		return
 	}
 
+	var normalized int = int(n % int64(ringSize-1))
+
 	var start *ring.Ring = r.Prev()
 	var removed = start.Unlink(1)
-	var target = start.Move(n)
+	var target = start.Move(normalized)
 	target.Link(removed)
 }
 
 func main() {
+	// Part 1
+	// var decryptionKey int64 = 1
+	// var numIterations int = 1
+
+	// Part 2
+	var decryptionKey int64 = 811589153
+	var numIterations int = 10
+
 	dat, err := os.ReadFile("input")
 	check(err)
 	lines := strings.Split(string(dat), "\n")
-	var numbers []int
+	var numbers []int64
 	for _, line := range lines {
 		if len(line) == 0 {
 			continue
@@ -46,7 +56,7 @@ func main() {
 		val, err := strconv.Atoi(line)
 		check(err)
 
-		numbers = append(numbers, val)
+		numbers = append(numbers, int64(val)*decryptionKey)
 	}
 
 	fmt.Printf("%d numbers in the list\n", len(numbers))
@@ -64,25 +74,29 @@ func main() {
 		buffer = buffer.Next()
 	}
 
-	for i, ptr := range pointers {
-		var shiftAmount = ptr.Value.(int)
-		Shift(ptr, shiftAmount, len(numbers))
+	for mixIter := 0; mixIter < numIterations; mixIter++ {
+		fmt.Printf("Mix iteration: %d\n", mixIter+1)
 
-		if shiftAmount != numbers[i] {
-			panic(fmt.Sprintf("Unexpected ptr value at index %d\n", i))
+		for i, ptr := range pointers {
+			var shiftAmount = ptr.Value.(int64)
+			Shift(ptr, shiftAmount, len(numbers))
+
+			if shiftAmount != numbers[i] {
+				panic(fmt.Sprintf("Unexpected ptr value at index %d\n", i))
+			}
 		}
 	}
 
-	var sum = 0
+	var sum int64 = 0
 	c := zero.Move(1000)
 	fmt.Printf("1000 after zero is %d\n", c.Value)
-	sum += c.Value.(int)
+	sum += c.Value.(int64)
 	c = c.Move(1000)
 	fmt.Printf("2000 after zero is %d\n", c.Value)
-	sum += c.Value.(int)
+	sum += c.Value.(int64)
 	c = c.Move(1000)
 	fmt.Printf("3000 after zero is %d\n", c.Value)
-	sum += c.Value.(int)
+	sum += c.Value.(int64)
 
 	fmt.Printf("Result is: %d\n", sum)
 }
