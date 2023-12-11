8935630982ad1fac1b9b9aec6a55a05d31147fc1
Ryan Kaneshiro <kaneshiro.ryan@gmail.com>
2023-12-10 17:01:02 -0800

    Add solution for Day 1 part 2

--- 2023/1/day1.py
+++ 2023/1/day1.py
@@ -6,10 +6,37 @@ def load_data(input_path: str):
         return [l for l in f.read().splitlines() if l.strip()]
 
 
+WORD_MAP: Dict[str, str] = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
+def replace_words(line: str) -> str:
+    replaced = []
+    for i in range(len(line)):
+        for literal, value in WORD_MAP.items():
+            if line[i:].startswith(literal):
+                replaced.append(value)
+                continue
+
+        replaced.append(line[i])
+
+    return "".join(replaced)
+
+
 def main(input_path):
     data = load_data(input_path)
     total = 0
     for line in data:
+        line = replace_words(line)
+        print(line)
         first_digit = None
         last_digit = None
         for c in line:
