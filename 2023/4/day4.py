e19283d4d3958d53748796359603e7255b53d5dc
Ryan Kaneshiro <kaneshiro.ryan@gmail.com>
2023-12-10 21:13:50 -0800

    Add solution for day 4, part 2

--- 2023/4/day4.py
+++ 2023/4/day4.py
@@ -1,5 +1,5 @@
 from typing import Iterable, Tuple
-from collections import Counter
+from collections import Counter, defaultdict
 import sys
 import re
 
@@ -7,22 +7,25 @@ import re
 def parse_numbers(number_text: str) -> list[int]:
     return [int(v) for v in re.split(r"\s+", number_text) if v]
 
-def load_data(input_path) -> Iterable[Tuple[list[int], list[int]]]:
+def load_data(input_path) -> Iterable[Tuple[int, list[int], list[int]]]:
     with open(input_path, "r") as f:
         for line in f.readlines():
             card, numbers = line.strip().split(":", 1)
+            _, card_num_text = card.split(" ", 1)
+            card_num = int(card_num_text)
             left, right = numbers.split("|", 1)
             winners = parse_numbers(left)
             in_hand = parse_numbers(right)
 
-            yield winners, in_hand
-
+            yield card_num, winners, in_hand
 
 
 def main(input_path):
-    total = 0 
+    card_counts: dict[int, int] = defaultdict(lambda: 1)
+    for card_num, winners, in_hand in load_data(input_path):
+        # How many cards of this number do we have
+        card_count = card_counts[card_num]
 
-    for winners, in_hand in load_data(input_path):
         winner_set = set(winners)
         counts = Counter(in_hand)
         total_matches = 0
@@ -30,11 +33,12 @@ def main(input_path):
             if val in winner_set:
                 total_matches += count
 
+        for card_nums in range(card_num + 1, card_num + total_matches + 1):
+            card_counts[card_nums] += card_count
 
-        if total_matches > 0:
-            total += pow(2, total_matches - 1)
-
-    print(total)
+    total_cards = sum(card_counts.values())
+    print(card_counts)
+    print(total_cards)
 
 
 if __name__ == "__main__":
