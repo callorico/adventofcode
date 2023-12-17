8516cd3f354cfb30dfc8044ac870b9cdd8aeabb7
Ryan Kaneshiro <kaneshiro.ryan@gmail.com>
2023-12-17 15:54:39 -0800

    Add solution for day 6, part 2
    Brute force seems to be good enough

--- 2023/6/day6.py
+++ 2023/6/day6.py
@@ -1,20 +1,24 @@
 import sys
+from typing import Tuple
 
 
 def parse_numbers(number_text: str) -> list[int]:
     return [int(n) for n in number_text.split(" ") if n]
 
+def parse_numbers2(number_text: str) -> int:
+    return int(number_text.replace(" ", ""))
 
-def load_data(input_path):
+
+def load_data(input_path) -> Tuple[int, int]:
     with open(input_path, "r") as f:
         time_line = f.readline()
         _, numbers = time_line.split(":", 2)
-        times = parse_numbers(numbers)
+        time = parse_numbers2(numbers)
 
         distance_line = f.readline()
         _, numbers = distance_line.split(":", 2)
-        distances = parse_numbers(numbers)
-        return times, distances
+        distance = parse_numbers2(numbers)
+        return time, distance
 
 
 def num_possible_wins(duration: int, distance: int) -> int:
@@ -31,17 +35,22 @@ def num_possible_wins(duration: int, distance: int) -> int:
 
 
 def main(input_path):
-    times, distances = load_data(input_path)
-    assert len(times) == len(distances)
-    num_races = len(times)
+    time, distance = load_data(input_path)
+    print(time)
+    print(distance)
 
-    combinations = 1
-    for i in range(num_races):
-        ways_to_win = num_possible_wins(times[i], distances[i])
-        print(f"# ways to win race {i} (duration: {times[i]}, distance: {distances[i]}): {ways_to_win}")
-        combinations *= ways_to_win
+    possible_wins = num_possible_wins(time, distance)
+    print(f"Total ways to win: {possible_wins}")
+    # assert len(times) == len(distances)
+    # num_races = len(times)
 
-    print(f"Total ways to win: {combinations}")
+    # combinations = 1
+    # for i in range(num_races):
+    #     ways_to_win = num_possible_wins(times[i], distances[i])
+    #     print(f"# ways to win race {i} (duration: {times[i]}, distance: {distances[i]}): {ways_to_win}")
+    #     combinations *= ways_to_win
+
+    # print(f"Total ways to win: {combinations}")
 
 
 if __name__ == "__main__":
