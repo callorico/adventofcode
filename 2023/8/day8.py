3761ad9391483e27bc62c77ff59d7ac9bea49720
Ryan Kaneshiro <kaneshiro.ryan@gmail.com>
2023-12-17 21:20:49 -0800

    Add solution for day 8, part 2

--- 2023/8/day8.py
+++ 2023/8/day8.py
@@ -1,6 +1,9 @@
 import sys
 from itertools import cycle
 from typing import Tuple
+from functools import reduce
+import operator
+import math
 
 
 def load_data(input_path) -> Tuple[str, dict[str, Tuple[str, str]]]:
@@ -19,15 +22,14 @@ def load_data(input_path) -> Tuple[str, dict[str, Tuple[str, str]]]:
     return instructions, nodes
 
 
-def main(input_path):
-    instructions, nodes = load_data(input_path)
-    print(instructions) 
-    print(nodes)
-
-    current = "AAA"
+def move_until_terminal(start: str, instructions: str, nodes: dict[str, Tuple[str, str]]) -> int:
+    current: str = start
     steps = 0
     moves = cycle(instructions)
-    while current != "ZZZ":
+
+    terminal_dist: dict[str, Tuple[str, int]] = {}
+
+    while True:
         move = next(moves)
         if move == "L":
             index = 0
@@ -39,7 +41,39 @@ def main(input_path):
         current = nodes[current][index]
         steps += 1
 
-    print(f"Took {steps} steps to reach desired state")
+        if current.endswith("Z"):
+            terminal_dist[start] = (current, steps)
+            if current in terminal_dist:
+                break
+
+            steps = 0
+            start = current
+
+    return terminal_dist
+
+
+def main(input_path):
+    instructions, nodes = load_data(input_path)
+
+    current = [s for s in nodes.keys() if s.endswith("A")]
+    print(f"starting states: {current}")
+
+    factors = []
+    for c in current:
+        factors.append(move_until_terminal(c, instructions, nodes))
+
+    print(factors)
+
+    # Conveniently, each starting state has just 1 associated
+    # terminal state and it takes the same number of steps
+    # to reach that terminal state each time. 
+    # 
+    # Here we just take the # of steps to the terminal states
+    # for each start state.
+    steps = [list(f.values())[0][1] for f in factors]
+    gcd = math.gcd(*steps)
+    steps = reduce(operator.mul, [s // gcd for s in steps]) * gcd
+    print(steps)
 
 
 if __name__ == "__main__":
