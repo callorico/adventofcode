7632fe7a95bb5d0a2d5b4da634304ba0bd10ec7c
Ryan Kaneshiro <kaneshiro.ryan@gmail.com>
2024-12-06 15:40:05 -0800

    Add solution for day 5 part 2

--- 2024/5/day5.py
+++ 2024/5/day5.py
@@ -37,20 +37,45 @@ def main(input_path):
     for x, y in orderings:
         constraints[x].add(y)
 
+    # Part 1
+    # total = 0
+    # for u in updates:
+    #     valid_update = True
+    #     for i in range(len(u)):
+    #         current = u[i]
+    #         after = set(u[i+1:])
+
+    #         must_be_after_current = constraints[current]
+    #         if after.difference(must_be_after_current):
+    #             valid_update = False
+    #             break
+    #     if valid_update:
+    #         total += midpoint(u)
+
+    # print(total)
+
+    # Part 2
     total = 0
     for u in updates:
-        valid_update = True
+        update_with_key = []
         for i in range(len(u)):
             current = u[i]
-            after = set(u[i+1:])
+            rest = list(u)
+            del rest[i]
 
             must_be_after_current = constraints[current]
-            if after.difference(must_be_after_current):
-                valid_update = False
-                break
+            # Count the number of constraint violations for the update value
+            # at the current position. The higher the number of violations, the
+            # farther to the right the value needs to be pushed. The constraint
+            # violation count is effectively the index position within the sorted
+            # list.
+            position = len(set(rest).difference(must_be_after_current))
+            update_with_key.append((current, position))
 
-        if valid_update:
-            total += midpoint(u)
+        sorted_update = [e[0] for e in sorted(update_with_key, key=lambda x: x[1])]
+        if sorted_update != u:
+            print(f"incorrectly ordered: {u}, sorted: {sorted_update}")
+            total += midpoint(sorted_update)
 
     print(total)
 
