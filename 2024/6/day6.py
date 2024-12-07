0988e049d7ad560ac3c6cb19a9cf85d50c031c41
Ryan Kaneshiro <kaneshiro.ryan@gmail.com>
2024-12-06 19:12:21 -0800

    Add solution for Day 6 part 2

--- 2024/6/day6.py
+++ 2024/6/day6.py
@@ -1,7 +1,8 @@
 import sys
 import re
-from typing import List, Tuple, Dict, Set
+from typing import List, Tuple, Dict, Set, Iterable
 from itertools import cycle
+from collections import defaultdict
 
 
 def load_data(input_path: str) -> Tuple[Tuple[int, int], List[str]]:
@@ -39,8 +40,7 @@ def is_blocked(guard_position: Tuple[int, int], grid: List[str]) -> bool:
     return grid[guard_position[0]][guard_position[1]] == "#"
 
 
-def main(input_path):
-    guard_position, grid = load_data(input_path)
+def run_guard(guard_position: Tuple[int, int], grid: List[str]) -> Tuple[bool, Iterable[Tuple[int, int]]]:
     rotations = cycle([
         # Up
         (-1, 0),
@@ -51,11 +51,17 @@ def main(input_path):
         # Left
         (0, -1)
     ])
-    visited_positions = set()
+
+    visited_positions: Dict[Tuple[int, int], Tuple[int, int]] = defaultdict(set)
     direction = next(rotations)
 
     while True:
-        visited_positions.add(guard_position)
+        if direction in visited_positions[guard_position]:
+            # Cycle detected
+            return True, visited_positions.keys()
+        else:
+            visited_positions[guard_position].add(direction)
+
         while True:
             proposed_next_position = (guard_position[0] + direction[0], guard_position[1] + direction[1])
             if is_blocked(proposed_next_position, grid):
@@ -67,7 +73,46 @@ def main(input_path):
             break
         guard_position = proposed_next_position
 
-    print(len(visited_positions))
+    return False, visited_positions.keys()
+
+
+def generate_new_grid(source_grid: List[str], row: int, col: int, new_value: str) -> List[str]:
+    assert row >= 0 and row < len(source_grid)
+    assert col >= 0 and col < len(source_grid[0])
+    assert len(new_value) == 1
+
+    row_to_modify = source_grid[row]
+    modified_row = row_to_modify[:col] + new_value + row_to_modify[col+1:]
+    copy = source_grid[:row] + [modified_row] + source_grid[row+1:]
+
+    return copy
+
+def main(input_path):
+    guard_position, grid = load_data(input_path)
+
+    # Part 1
+    _, visited = run_guard(guard_position, grid)
+    print(len(visited))
+
+    # Part 2
+    candidates = set(visited)
+    for r, c in visited:
+        candidates.add((r + 1, c))
+        candidates.add((r - 1, c))
+        candidates.add((r, c + 1))
+        candidates.add((r, c - 1))
+
+    loop_positions = 0
+    for r, c in candidates:
+        if in_bounds((r, c), grid) and grid[r][c] == ".":
+            # Create a proposed grid with an obstacle at this position
+            proposed_grid = generate_new_grid(grid, r, c, "#")
+            cycle, _ = run_guard(guard_position, proposed_grid)
+            if cycle:
+                loop_positions += 1
+
+    print(loop_positions)
+
 
 
 if __name__ == '__main__':
