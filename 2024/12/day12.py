d01f0737b475a97093b72f24ccb410e6ffd70bdf
Ryan Kaneshiro <kaneshiro.ryan@gmail.com>
2024-12-21 16:05:32 -0800

    Add solution to day 12 part 2
    Yuck

--- 2024/12/day12.py
+++ 2024/12/day12.py
@@ -47,6 +47,104 @@ def perimeter(region: List[Tuple[int, int]]) -> int:
     return total
 
 
+def sides(region: List[Tuple[int, int]]) -> int:
+    area_lookup: Set[Tuple[int, int]]= set(region)
+    first = region[0]
+    min_row = max_row = first[0]
+    min_col = max_col = first[1]
+
+    for r, c in region[1:]:
+        if r < min_row:
+            min_row = r
+        elif r > max_row:
+            max_row = r
+
+        if c < min_col:
+            min_col = c
+        elif c > max_col:
+            max_col = c
+
+    #print(f"Region: {area_lookup}")
+    #print(f"Region bounds: [{min_row},{min_col}] to [{max_row},{max_col}]")
+
+    # Idea: Iterate through grid in horizontal lines and
+    # vertical lines and count # of contiguous segments
+
+    total_sides = 0
+    # Iterate through all the horizontal lines and
+    # count sides above and below
+    within_above_side = False
+    within_below_side = False
+    for r in range(min_row, max_row + 1):
+        # Extend one column out past the right edge of the region to terminate
+        # any sides that extend to the end.
+        for c in range(min_col, max_col + 2):
+            in_region = (r, c) in area_lookup
+            if in_region:
+                # Check above
+                if (r-1, c) in area_lookup:
+                    if within_above_side:
+                        total_sides += 1
+                    within_above_side = False
+                else:
+                    within_above_side = True
+
+                # Check below
+                if (r+1, c) in area_lookup:
+                    if within_below_side:
+                        total_sides += 1
+                    within_below_side = False
+                else:
+                    within_below_side = True
+            else:
+                # Any currently active side is ended
+                if within_above_side:
+                    total_sides += 1
+                within_above_side = False
+
+                if within_below_side:
+                    total_sides += 1
+                within_below_side = False
+
+
+    # Iterate through all the vertical lines and count
+    # sides to the left and right
+    within_left_side = False
+    within_right_side = False
+    for c in range(min_col, max_col + 1):
+        # Extend one row out past the bottom edge of the region to terminate
+        # any sides that extend to the bottom
+        for r in range(min_row, max_row + 2):
+            in_region = (r, c) in area_lookup
+            if in_region:
+                # Check left
+                if (r, c-1) in area_lookup:
+                    if within_left_side:
+                        total_sides += 1
+                    within_left_side = False
+                else:
+                    within_left_side = True
+
+                # Check right
+                if (r, c+1) in area_lookup:
+                    if within_right_side:
+                        total_sides += 1
+                    within_right_side = False
+                else:
+                    within_right_side = True
+            else:
+                # Any currently active side is ended
+                if within_left_side:
+                    total_sides += 1
+                within_left_side = False
+
+                if within_right_side:
+                    total_sides += 1
+                within_right_side = False
+
+    return total_sides
+
+
 def area(region: List[Tuple[int, int]]) -> int:
     return len(region)
 
@@ -63,9 +161,13 @@ def main(input_path):
             if (r, c) not in visited:
                 region = find_region(grid, visited, r, c, plant)
                 a = area(region)
-                p = perimeter(region)
-                price = a * p
-                print(f"A region of {plant} plants with price {a} * {p}: {price}")
+                # Part 1
+                #p = perimeter(region)
+                #price = a * p
+                # Part 2
+                s = sides(region)
+                price = a * s
+                print(f"A region of {plant} plants with price {a} * {s}: {price}")
                 total_price += price
 
     print(total_price)
