99f8c7e592f8c584a942ab0fdce6d077f3ca4b67
Ryan Kaneshiro <kaneshiro.ryan@gmail.com>
2024-12-30 13:07:24 -0800

    Add solution for day 19 part 2

--- 2024/19/day19.py
+++ 2024/19/day19.py
@@ -15,6 +15,28 @@ def load_data(input_path: str) -> Tuple[List[str], List[str]]:
     return patterns, targets
 
 
+def pattern_counts(patterns: List[str], target: str, cache: Dict[str, int]) -> int:
+    if not target:
+        return 0
+
+    previously_cached = cache.get(target)
+    if previously_cached is not None:
+        return previously_cached
+
+    matches = 0
+    for pattern in patterns:
+        if target.startswith(pattern):
+            remaining_target = target[len(pattern):]
+            if not remaining_target:
+                matches += 1
+            else:
+                matches += pattern_counts(patterns, remaining_target, cache)
+
+    # print(f"{target} has {matches} matches.")
+    cache[target] = matches
+    return matches
+
+
 def is_possible(patterns: List[str], target: str, cache: Dict[str, bool]) -> bool:
     if not target:
         return True
@@ -36,14 +58,23 @@ def is_possible(patterns: List[str], target: str, cache: Dict[str, bool]) -> boo
 
 def main(input_path: str):
     patterns, targets = load_data(input_path)
+    print(patterns)
+
+    # is_possible_cache = {}
+    # candidates = []
+    # for target in targets:
+    #     if is_possible(patterns, target, is_possible_cache):
+    #         candidates.append(target)
 
     cache = {}
-    possible = 0
+    total_patterns = 0
     for target in targets:
-        if is_possible(patterns, target, cache):
-            possible += 1
+        # print(f"--- Evaluating {target} ---")
+        count = pattern_counts(patterns, target, cache)
+        print(f"{target} has {count} possible arrangements.")
+        total_patterns += count
 
-    print(possible)
+    print(total_patterns)
 
 
 if __name__ == "__main__":
